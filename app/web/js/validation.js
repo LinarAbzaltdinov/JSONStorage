2e3aa952446dcc75a7842fde9c350da7bc9a17dc
linarkou <abzaltdinov@gmail.com>
2018-03-23 19:31:48 +0300

    added cron

--- app/web/js/validation.js
+++ app/web/js/validation.js
@@ -6,3 +6,19 @@ function isValidJSON(string) {
     }
     return true;
 }
+
+function isValidDateTime(minutes, hours, day, month, year) {
+    var date = new Date(year, month, day, hours, minutes);
+    //check if autoincreased
+    if ((date.getMinutes() != minutes)
+        || (date.getHours() != hours)
+        || (date.getDate() != day)
+        || (date.getMonth() != month)
+        || (date.getFullYear() != year)) {
+        return false;
+    }
+    if (date < Date.now()) {
+        return false;
+    }
+    return true;
+}
---
+++ config/postgresql/setconfig.sh
@@ -0,0 +1,2 @@
+echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
+echo "cron.database_name = 'postgres'" >> /var/lib/postgresql/data/postgresql.conf
